7a88e5578b6a6b27f85a6b35e3bf13314a4af4ae
Agwara <nnaemekaagwara@gmail.com>
2025-01-17 12:15:19 +0100

    Added mock data integration

--- src/components/DashboardPages/Home/Home.jsx
+++ src/components/DashboardPages/Home/Home.jsx
@@ -2,13 +2,21 @@
 import React, {useState} from "react";
 
 import styles from "./styles.module.css";
+import TableControl from "../../TableControl/TableControl";
+import Table from "../../Table/Table";
+const LISTONE = ["All", "Draft", "Live", "Past", "Scheduled"]
 
 const Home = () => {
 
+  const [selected, setSelected] = useState(LISTONE[0])
+
   return (
     <div className={styles.container}>
-      <h3 className={styles.headerText}>Dashboard</h3>
+      <h3 className={styles.headerText}>Stories</h3>
 
+      <TableControl setSelected={setSelected} selected={selected} list={LISTONE} />
+
+      <Table status={selected} />
     </div>
   )
 }
