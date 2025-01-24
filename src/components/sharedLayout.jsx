b58090a549617c4c2018266730d19e2b17ac4c12
oghene-kevwe <idisepraise@gmail.com>
2025-01-24 01:04:21 +0100

    added global context
    Signed-off-by: oghene-kevwe <idisepraise@gmail.com>

--- src/components/sharedLayout.jsx
+++ src/components/sharedLayout.jsx
@@ -1,9 +1,24 @@
 import { Outlet } from "react-router-dom";
+import { NavBar } from "./navbar";
+import { Footer } from "./footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useGlobalContext } from "../utils/context";
+import { GlobalLoadingIcon } from "./globalLoading";
 
 export const SharedLayout = () => {
+  const { globalLoading } = useGlobalContext();
   return (
-    <div>
-      <Outlet />
+    <div className=" flex flex-col h-screen">
+      {globalLoading && <GlobalLoadingIcon />}
+      <div className=" px-[2vw] md:px-[4vw] pt-4">
+        <ToastContainer theme="dark" autoClose={3000} />
+        <NavBar />
+        <Outlet />
+      </div>
+      <div className=" mt-auto">
+        <Footer />
+      </div>
     </div>
   );
 };
