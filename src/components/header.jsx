a7b91dbd799a7eccf787e9a0c9b89befe1a6e582
oghene-kevwe <idisepraise@gmail.com>
2025-01-24 01:03:33 +0100

    adjusted component styles
    Signed-off-by: oghene-kevwe <idisepraise@gmail.com>

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,8 @@
-export const header = () => {
+export const HeaderComponent = ({ title, subtitle }) => {
   return (
-    <div>header</div>
-  )
-}
+    <div className=" mb-8 bg-black/5 rounded-sm p-3">
+      <h1 className=" mb-4 text-black font-medium text-lg">{title}</h1>
+      <p>{subtitle}</p>
+    </div>
+  );
+};
