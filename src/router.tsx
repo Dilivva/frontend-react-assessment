add105cc927d5f363587fbc65018b64e7e2e3ceb
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2025-01-27 07:16:29 +0100

    FEAT: Update TypeScript types in router and upgrade @types/node package

--- src/router.tsx
+++ src/router.tsx
@@ -1,28 +1,48 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import { createHashRouter, RouteObject } from 'react-router-dom'
 import ErrorPage from './components/error-page'
 import { getDefaultLayout } from './components/layout'
 import HomePage from './pages/home'
 
-export const routerObjects: RouteObject[] = [
+// Define a custom RouteObject type to handle your specific requirements
+interface CustomRouteObject extends Omit<RouteObject, 'children'> {
+  Component?: React.FC & { getLayout?: (page: ReactElement) => ReactElement }
+  children?: CustomRouteObject[]
+}
+
+export const routerObjects: CustomRouteObject[] = [
   {
     path: '/',
     Component: HomePage,
+    children: [],
   },
 ]
 
 export function createRouter(): ReturnType<typeof createHashRouter> {
-  const routeWrappers = routerObjects.map((router) => {
-    // @ts-ignore TODO: better type support
+  const routeWrappers = routerObjects.map((router): RouteObject => {
+    const children = router.children?.map((child): RouteObject => {
+      const getLayout = child.Component?.getLayout || getDefaultLayout
+      const ChildComponent = child.Component!
+      return {
+        ...child,
+        element: getLayout(<ChildComponent />),
+        Component: undefined, // Ensure this field is not passed to the router
+        ErrorBoundary: ErrorPage,
+      }
+    })
+
     const getLayout = router.Component?.getLayout || getDefaultLayout
     const Component = router.Component!
     const page = getLayout(<Component />)
+
     return {
       ...router,
       element: page,
-      Component: null,
+      Component: undefined, // Ensure this field is not passed to the router
       ErrorBoundary: ErrorPage,
+      children, // Attach child routes
     }
   })
+
   return createHashRouter(routeWrappers)
 }
