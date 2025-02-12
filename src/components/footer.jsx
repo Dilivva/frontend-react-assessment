99fb3e07deeacef929a326facd935606594ac2c3
OYERINDE Enoch <57858245+EnochOluwadamilare@users.noreply.github.com>
2025-02-12 09:33:34 +0100

    Revert "Praise idise assessment"

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className=" mt-8 bg-black/5 rounded-sm p-3 text-center">
-      <div>
-        <p>&copy; {new Date().getFullYear()} SportsHub</p>
-      </div>
-    </footer>
-  );
-};
